6436801ace5e0d29312947836b4dd459b2a3c40d
jOkhrimenko <janeokhrimenko@gmail.com>
2019-10-03 14:50:14 +0200

    zajecia1

---
+++ Podstawy programowania/zajęcia_1/zajecia_1/zajecia_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zajecia_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //komenarz
+
+            /*komentarz
+             * w
+             * wielu liniach
+             /*gvgvgvug*/
+            Console.WriteLine("Janusz");
+            Console.ReadKey();
+
+            //zmiene i typy danych 
+            int x =10;
+            x = 15;
+            Console.WriteLine(x);
+
+            int x1, y = 10, z = 15;
+            x1 = 2;
+
+             Console.WriteLine("zmienna x1 wynosi: ");
+             Console.WriteLine(x1);
+
+            //konkakenacja
+            Console.WriteLine("y=" + y + "\nz =" + z);
+
+            int a = 10;
+            int b = a;
+            Console.WriteLine(a + "" + b);
+
+            //alternatywny sposób derlaracji zmiennej
+            //int c =0;
+
+            int c = new int(); //wartość domyśla ==> null (brak referencji)
+
+            Console.WriteLine("Podaj swoje imię:");
+            string name = Console.ReadLine("");
+            Console.WriteLine("Twoje imię podane z klawiatury: " + name);
+
+            int zl = 12;
+
+            int zl = 20;
+
+            float y1 = 10.55F;
+
+
+            //oblicz pole prostokąta, dane uzytkownik podaje z klawiatyru 
+
+            Console.WriteLine( "Podaj  bok a:"); 
+            int bok1 =int.
+        }
+    }
+}
